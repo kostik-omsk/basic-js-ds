55f9cb5251e1d939f9a15e36fd71432e6b75afab
sidorenko konstantin <kosiak5587@gmail.com>
2023-04-15 15:09:34 +0600

    feat: add solution removeKFromList

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,7 +1,8 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
+const { queue } = require('./queue.js');
 /**
  * Given a singly linked list of integers l and an integer k,
  * remove all elements from list l that have a value equal to k.
@@ -22,11 +23,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  let current = l;
+  let prev = null;
+
+  while (current) {
+    //Проверяем, что current не равен null
+    if (current.value === k) {
+      //Проверяем, равно ли значение текущего элемента k
+      if (prev) {
+        //prev существует (то мы не удаляем первый элемент списка)мы связываем prev.next с current.next
+        prev.next = current.next;
+      } else {
+        // удаляем первый элемент списка если он был равен k, устанавливая его равным current.next.
+        l = current.next;
+      }
+    } else {
+      /**
+       *не равно k, мы просто обновляем prev, устанавливая его равным current
+       *для связывания предыдущего элемента со следующим элементом,
+       *если текущий элемент не будет удален
+       */
+      prev = current;
+    }
+    /**
+     * Обновляем current, устанавливая его равным current.next, чтобы перейти к следующему элементу в списке.
+     * И так пока current не станет равным null
+     * что означает, что мы прошли весь список.
+     */
+
+    current = current.next;
+  }
+  return l;
 }
 
 module.exports = {
-  removeKFromList
+  removeKFromList,
 };
